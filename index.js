4b248bb4f87accfdd56cb8c8dc7e7c5e39c95136
Janosch Schwalm <janosch.schwalm@gmail.com>
2022-01-28 10:42:35 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-function changeLetters() {
+function changeLetters(event) {
 
-  var textarea = document.getElementById('inputtext');
+  e.stopPropagation();
+  e.preventDefault();
+
+  // Get pasted data via clipboard API
+  var clipboardData = e.clipboardData || window.clipboardData;
+  var pastedData = clipboardData.getData('Text');
 
 
-  changeToRandomUTF = textarea.value.replaceAll('l', '񅪜')
+
+  changeToRandomUTF = pastedData.replaceAll('l', '񅪜')
   changeToRandomUTF = changeToRandomUTF.replaceAll('L', 'ѯ')
   changeToRandomUTF = changeToRandomUTF.replaceAll('r', '𫍪')
   changeToRandomUTF = changeToRandomUTF.replaceAll('R', '񣙧')
@@ -14,5 +20,5 @@ function changeLetters() {
   changeBack = changeBack.replaceAll('𫍪', 'l')
   changeBack = changeBack.replaceAll('񣙧', 'L')
 
-  textarea.value=changeBack
+  document.getElementById('inputtext').value = changeBack
 }
