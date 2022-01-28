b388aa00d8d094507eea769524c23ee5c3188e7c
Janosch Schwalm <janosch.schwalm@gmail.com>
2022-01-28 10:17:45 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+var textarea = document.getElementById('inputtext');
+
+
+changeToRandomUTF = textarea.value.replaceAll('l', '񅪜')
+changeToRandomUTF = changeToRandomUTF.replaceAll('L', 'ѯ')
+changeToRandomUTF = changeToRandomUTF.replaceAll('r', '𫍪')
+changeToRandomUTF = changeToRandomUTF.replaceAll('R', '񣙧')
+
+
+changeBack = changeToRandomUTF.replaceAll('񅪜', 'r')
+changeBack = changeBack.replaceAll('ѯ', 'R')
+changeBack = changeBack.replaceAll('𫍪', 'l')
+changeBack = changeBack.replaceAll('񣙧', 'L')
+
+textarea.value=changeBack
